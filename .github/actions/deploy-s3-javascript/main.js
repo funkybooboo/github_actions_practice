96f1c579359c7474fbaa859a41f3fa638f1b44d7
Nate Stott <nate.stott@pm.me>
2024-09-08 16:55:53 -0600

    really deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-function run() {
+async function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFoler = core.getInput('dist-folder', {required: true});
+
+    // 2) upload files
+    const s3Uri = `s3://${bucket}`;
+
+    await exec.exec(`aws s3 sync ${distFoler} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
