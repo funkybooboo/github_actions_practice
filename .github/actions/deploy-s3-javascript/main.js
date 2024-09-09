9c946170f1529c5d8f25497a9eaddf6a910c2eda
Nate Stott <nate.stott@pm.me>
2024-09-08 21:49:17 -0600

    print website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,8 @@ async function run() {
 
     await exec.exec(`aws s3 sync ${distFoler} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
